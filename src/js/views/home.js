b3193be038e2f5fbddf4d7a9900eb48143dd4258
Alvaro Ponce <alvaroponce@iMac-de-Alvaro-2.local>
2024-03-18 11:56:41 +0100

    proyecto empezado

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,15 @@
 import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/react.png";
 import "../../styles/home.css";
 
 export const Home = () => (
 	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
+		<h1>Hello React Router</h1>
 		<p>
 			<img src={rigoImage} />
 		</p>
 		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
+			Si ves este botón no se ha roto nada
 		</a>
 	</div>
 );
